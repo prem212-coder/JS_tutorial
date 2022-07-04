dd6605db70d099451cdbbf0f1d741be2216f59a6
Prem Mohan <212premy@gmail.com>
2022-07-04 23:50:08 +0530

    first commit

---
+++ testTipCalculator.js
@@ -0,0 +1,8 @@
+let bill = 275;
+const result = 50 <= 300 ? (bill * 15) / 100 : (bill * 20) / 100;
+
+console.log(
+  `The bill was ${bill} The tip was${result}and The total valuse is ${
+    bill + result
+  }`
+);
